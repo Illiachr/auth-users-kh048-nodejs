39f611f5d2e2d30b7bf88b48e06b15bbbb613478
timotiaztec <timotiaztec@gmail.com>
2021-04-03 22:11:52 +0300

    Commit to merge

--- sql/authdb.sql
+++ sql/authdb.sql
@@ -1,12 +1,14 @@
-CREATE TABLE roles (
-  id SERIAL PRIMARY KEY,
-  role VARCHAR(50) NOT NULL
+CREATE TABLE roles
+(
+    id   SERIAL PRIMARY KEY,
+    role VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE users (
-  id uuid NOT NULL,
-	role_id INTEGER REFERENCES roles(id) NOT NULL,
-  login VARCHAR(255) PRIMARY KEY NOT NULL,
-  hash VARCHAR(255) NOT NULL,
-	salt VARCHAR(255) NOT NULL
+CREATE TABLE users
+(
+    id uuid NOT NULL,
+    role_id INTEGER REFERENCES roles (id) NOT NULL,
+    login   VARCHAR(255) PRIMARY KEY      NOT NULL,
+    hash    VARCHAR(255)                  NOT NULL,
+    salt    VARCHAR(255)                  NOT NULL
 );

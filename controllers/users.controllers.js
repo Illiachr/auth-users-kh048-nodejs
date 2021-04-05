c3c2023cae48718c7e05e94073838ac2c8966c89
PING LORD <timotiaztec@gmail.com>
2021-04-05 13:50:52 +0300

    Match spec routes fix (#5)
    * Commit to merge
    
    * Made change-password and change-role routes, and some small additions, such as token check middleware, etc
    
    * Added route for all users and made small fix
    
    * Remade routes to match specification

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -11,4 +11,29 @@ const signUp = async (req, res, next) => {
   next();
 };
 
-module.exports = { signUp, signIn };
+const changePassword = async (req, res, next) => {
+  const { password, newPassword } = req.body;
+  const id = req.params.id;
+  await service.changePassword(id, password, newPassword, res);
+  next();
+};
+
+const changeRole = async (req, res, next) => {
+  const { newRole } = req.body;
+  const id = req.params.id;
+  await service.changeRole(id, newRole, res);
+  next();
+};
+
+const getAll = async (req, res, next) => {
+  await service.getAll(res);
+  next();
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  changeRole,
+  getAll
+};
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,11 +1,21 @@
 const { Router } = require('express');
 const router = Router();
 const controller = require('../controllers/users.controllers');
+const { checkToken } = require('../_helpers/auth.utils');
 
 router.route('/signup')
   .post(controller.signUp);
 
 router.route('/signin')
   .post(controller.signIn);
+
+router.route('/:id/change-password')
+  .patch(checkToken(['Admin', 'User']), controller.changePassword);
+
+router.route('/:id/change-role')
+  .patch(checkToken(['Admin']), controller.changeRole);
+
+router.route('/')
+  .get(checkToken(['Admin']), controller.getAll);
 
 module.exports = router;
